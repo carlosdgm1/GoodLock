41b3f2ea0e4ad64342b42d8ef6134fca2fa76ed7
carlosdgm1 <70540565+carlosdgm1@users.noreply.github.com>
2021-10-13 21:30:42 -0500

    Seeder, migracion, cambiar a 1

--- app/Http/Controllers/Arduino.php
+++ app/Http/Controllers/Arduino.php
@@ -4,16 +4,26 @@ namespace App\Http\Controllers;
 
 use App\Models\Arduino as ModelsArduino;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class Arduino extends Controller
 {
     public function update($id){
 
-        $a = ModelsArduino::find($id);
-        $a->estatus = "1";
-        $a->save();
+       
 
-         
+
+        $b = ModelsArduino::where('id','<>',$id)->first();
+        
+        $a = ModelsArduino::find($id);
+
+        $a->estatus = "1";
+        $b->estatus = "0";
+
+        
+        $a->save();
+        $b->save();
+        
         return redirect()->back();
     }
 }
